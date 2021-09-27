4e0dfcd9c37f384bd3ca05065094344fff2e93a1
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-09-27 17:41:05 -0500

    Add folder for pages and move the main page

--- SimpleNotesApp/SimpleNotes/SimpleNotes/App.xaml.cs
+++ SimpleNotesApp/SimpleNotes/SimpleNotes/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SimpleNotes.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ SimpleNotesApp/SimpleNotes/SimpleNotes/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleNotes.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
